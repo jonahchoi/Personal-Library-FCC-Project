35e1845e094c81439a96f24e3748e0947d6b5af4
jonahchoi <104597125+jonahchoi@users.noreply.github.com>
2022-05-18 22:45:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
+
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
